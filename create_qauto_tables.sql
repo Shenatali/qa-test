38f0393bed12f82c18b9147ab98a07ec05636d6e
nata <natali_shender@ukr.net>
2024-04-15 17:01:43 +0300

    update varchar to char and float

--- create_qauto_tables.sql
+++ create_qauto_tables.sql
@@ -1,25 +1,25 @@
 CREATE schema qauto_schema;
 CREATE table qauto_schema.car_brands(
-id varchar(255),
+id int,
 title varchar(255)
 );
 CREATE table qauto_schema.car_models(
-id varchar(255),
-carBrandId varchar(255),
+id int,
+carBrandId int,
 title varchar(255)
 );
 CREATE table qauto_schema.users(
-id varchar(255),
+id int,
 firstName varchar(255),
 lastName varchar(255),
 email varchar(255),
 password varchar(255)
 );
 CREATE table qauto_schema.cars(
-id varchar(255),
-userrId varchar(255),
-carBrandId varchar(255),
-carModelId varchar(255),
-mileage varchar(255),
-initialMileage varchar(255)
+id int,
+userrId int,
+carBrandId int,
+carModelId int,
+mileage float,
+initialMileage float
 );
